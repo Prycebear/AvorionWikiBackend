a1e8680c10f50487dbeeffa7e722e9e34af72a26
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-09-01 17:09:46 +0100

    DB change

--- src/main/java/com/example/dndavorionwikibackend/Model/FantayRole/FantasyRole.java
+++ src/main/java/com/example/dndavorionwikibackend/Model/FantayRole/FantasyRole.java
@@ -1,19 +1,11 @@
 package com.example.dndavorionwikibackend.Model.FantayRole;
 
-import com.example.dndavorionwikibackend.Model.Characters.CharacterSuper;
-import com.example.dndavorionwikibackend.Model.Characters.NonPlayerCharacter;
-import com.example.dndavorionwikibackend.Model.Characters.PlayerCharacter;
-import com.example.dndavorionwikibackend.Model.History.History;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import java.util.Set;
@@ -27,10 +19,4 @@ public class FantasyRole {
     @Column(name = "ROLE_ID", nullable = false, unique = true)
     private long roleId;
 
-//    @ManyToMany
-//    @JoinTable(
-//            name = "PLAYER_CHARACTER_ROLE",
-//            joinColumns = @JoinColumn(name = "CHARACTER_ID"),
-//            inverseJoinColumns = @JoinColumn(name = "WORLD_DATE_ID"))
-//    private Set<PlayerCharacter> histories;
 }
