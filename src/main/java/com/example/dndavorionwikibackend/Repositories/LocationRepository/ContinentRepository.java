51821cdd766f2b5291824e995746abe7778b67d3
Prycebear <90221801+Prycebear@users.noreply.github.com>
2023-09-10 22:04:54 +0100

    Fixed Continent controller.

--- src/main/java/com/example/dndavorionwikibackend/Repositories/LocationRepository/ContinentRepository.java
+++ src/main/java/com/example/dndavorionwikibackend/Repositories/LocationRepository/ContinentRepository.java
@@ -2,6 +2,8 @@ package com.example.dndavorionwikibackend.Repositories.LocationRepository;
 
 import com.example.dndavorionwikibackend.Model.Locations.Continent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContinentRepository extends JpaRepository<Continent, Long> {
 }
